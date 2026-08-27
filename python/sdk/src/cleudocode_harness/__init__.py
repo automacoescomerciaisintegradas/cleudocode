62d92d1e466cfe4ae7d22f5657621031dc22fdee
automacoescomerciaisintegradas <automacoescomerciais@gmail.com>
2026-08-28 00:18:26 +0200

    feat: Add cleudocode_harness Python SDK and GitLab CI release workflow

---
+++ python/sdk/src/cleudocode_harness/__init__.py
@@ -0,0 +1,19 @@
+from .api import CleudocodeHarness, CleudocodeHarnessConfig, RunResult, Session
+from .client import HarnessClient, HarnessConfig
+from .errors import SdkProtocolError
+from .models import IncomingRequest, InitializeResponse, JsonObject, Notification, ServerInfo
+
+__all__ = [
+    "CleudocodeHarness",
+    "CleudocodeHarnessConfig",
+    "Session",
+    "RunResult",
+    "HarnessClient",
+    "HarnessConfig",
+    "SdkProtocolError",
+    "IncomingRequest",
+    "InitializeResponse",
+    "JsonObject",
+    "Notification",
+    "ServerInfo",
+]
